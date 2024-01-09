d276ab54b7bbef6d74efe60004207058b8ee39aa
abdelrahman yasser mohamed <badyyasser2@gmail.com>
2024-01-09 12:29:25 +0200

    style

--- js.js
+++ js.js
@@ -99,20 +99,37 @@ function getQueryStringParams() {
 
 // Function to make an AJAX request using the Fetch API
 function ajaxRequest(options) {
-  return fetch("your-api-endpoint", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(options),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        throw data.error;
-      }
-      return data;
-    });
+  return fetch(
+    options.host || "https://www.7ader.net/GeneralApiV11/api/General/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(options.headers || {}),
+      },
+      body: JSON.stringify({
+        ConnectionStringName: options.ConnectionStringName || "Teleware",
+        StoredProcedureName: options.SP,
+        SpParams: options.SqlParams,
+        CacheKey: options.cacheKey || null,
+        TablesNames: options.TablesNames || null,
+        RenameTables: options.renameTables || false,
+        IncludesUserID: options.IncludesUserID || false,
+      }),
+    }
+  ).then((response) => {
+    if (response.ok === true && response.status === 200) {
+      return response.json().then((res) => {
+        if (isNaN(res["error-code"]) || res["error-code"] === 4) {
+          return res;
+        } else {
+          throw new Error("API Error: " + res["error-code"]);
+        }
+      });
+    } else {
+      throw new Error("HTTP Error: " + response.status);
+    }
+  });
 }
 
 // Your main logic
@@ -129,6 +146,5 @@ ajaxRequest({
     console.log(data);
     const userData = data.table1[0];
     const socialMediaLinks = data.table2;
-    renderApp();
   })
   .catch((error) => console.log(error));
